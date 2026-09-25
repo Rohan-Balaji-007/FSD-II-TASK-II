0a8a53425f855944ea464344c8e36945164e30d0
Rohan-Balaji-007 <rohanbalaji25@gmail.com>
2026-09-25 11:29:55 +0530

    Remove Counter component from App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,1 @@
-import React, { useState } from 'react';
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = () => {
-    setCount(prevCount => prevCount + 1);
-  };
-
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Counter App</h1>
-      <p>Current Count: <strong>{count}</strong></p>
-      <button onClick={incrementCount}>Increment</button>
-    </div>
-  );
-};
-
-export default Counter;
